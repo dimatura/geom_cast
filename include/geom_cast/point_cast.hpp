b96d679bc90d6daf96019c0a12985abaa3ebf8ca
dimatura <dimatura@9e839a59-819f-469c-a56d-6f7bff314291>
2013-08-18 03:32:48 +0000

    added more point types
    git-svn-id: svn+ssh://panda.frc.ri.cmu.edu/projects/aeroscout/software/repository/trunk/ca_stacks/src/ca_common/geom_cast@5569 9e839a59-819f-469c-a56d-6f7bff314291

--- include/geom_cast/point_cast.hpp
+++ include/geom_cast/point_cast.hpp
@@ -1,5 +1,5 @@
-#ifndef POINT_CONVERSION_HPP_LZDTPRFM
-#define POINT_CONVERSION_HPP_LZDTPRFM
+#ifndef POINT_CAST_HPP_EGWZH108
+#define POINT_CAST_HPP_EGWZH108
 
 #include <boost/utility/enable_if.hpp>
 #include <boost/type_traits/integral_constant.hpp>
@@ -12,9 +12,12 @@
 
 #include <tf/tf.h>
 
+#include <opencv2/core/core.hpp>
+
 namespace ca
 {
 
+///////////////////////////////////////////////////////////////////////////////
 // point set/get traits
 
 template<typename T>
@@ -29,14 +32,20 @@ struct xyz_ctor_set : public boost::false_type { };
 template<typename T>
 struct xyz_tfget_get : public boost::false_type { };
 
+template<typename T>
+struct xyz_array_get : public boost::false_type { };
+
+///////////////////////////////////////////////////////////////////////////////
 // trait implementations for various point types
 
+// tf::Vector3
 template<>
 struct xyz_tfget_get<tf::Vector3> : public boost::true_type { };
 
 template<>
 struct xyz_ctor_set<tf::Vector3> : public boost::true_type { };
 
+// pcl
 template<>
 struct xyz_member_getset<pcl::PointXYZ> : public boost::true_type { };
 
@@ -52,9 +61,14 @@ struct xyz_member_getset<pcl::PointXYZL> : public boost::true_type { };
 template<>
 struct xyz_member_getset<pcl::PointWithViewpoint> : public boost::true_type { };
 
+// geometry_msgs
 template<>
 struct xyz_member_getset<geometry_msgs::Point> : public boost::true_type { };
 
+template<>
+struct xyz_member_getset<geometry_msgs::Vector3> : public boost::true_type { };
+
+// eigen
 template<class Scalar>
 struct xyz_array_getset<Eigen::Matrix<Scalar, 3, 1> > : public boost::true_type { };
 
@@ -67,6 +81,21 @@ struct xyz_array_getset<Eigen::Map<const Eigen::Matrix<Scalar, 3, 1> > > : publi
 template<class Scalar>
 struct xyz_array_getset<Eigen::Map<const Eigen::Matrix<Scalar, 4, 1> > > : public boost::true_type { };
 
+// raw pointer. a bit dangerous since it matches any pointer.
+// TODO restrict to numeric types. No boundary checks either.
+template<class Scalar>
+struct xyz_array_get<Scalar *> : public boost::true_type { };
+
+// raw arrays. a bit dangerous since it matches any array.
+// TODO restrict to numeric types.
+template<class Scalar>
+struct xyz_array_getset<Scalar [3]> : public boost::true_type { };
+
+// opencv
+template<class Scalar>
+struct xyz_member_getset<cv::Point3_<Scalar> > : public boost::true_type { };
+
+///////////////////////////////////////////////////////////////////////////////
 // non-exhaustive set of converters for various src/target combinations.
 // TODO add useful converters as needed.
 
@@ -108,7 +137,31 @@ Target point_cast(const Source& src,
 
 template<typename Target, typename Source>
 Target point_cast(const Source& src,
+                  typename boost::enable_if< xyz_array_get<Source> >::type* dummy1 = 0,
+                  typename boost::enable_if< xyz_array_getset<Target> >::type* dummy2 = 0
+                 ) {
+  Target tgt;
+  tgt[0] = src[0];
+  tgt[1] = src[1];
+  tgt[2] = src[2];
+  return tgt;
+}
+
+template<typename Target, typename Source>
+Target point_cast(const Source& src,
                   typename boost::enable_if< xyz_array_getset<Source> >::type* dummy1 = 0,
+                  typename boost::enable_if< xyz_member_getset<Target> >::type* dummy2 = 0
+                 ) {
+  Target tgt;
+  tgt.x = src[0];
+  tgt.y = src[1];
+  tgt.z = src[2];
+  return tgt;
+}
+
+template<typename Target, typename Source>
+Target point_cast(const Source& src,
+                  typename boost::enable_if< xyz_array_get<Source> >::type* dummy1 = 0,
                   typename boost::enable_if< xyz_member_getset<Target> >::type* dummy2 = 0
                  ) {
   Target tgt;
@@ -166,6 +219,14 @@ Target point_cast(const Source& src,
   return Target(src[0], src[1], src[2]);
 }
 
+template<typename Target, typename Source>
+Target point_cast(const Source& src,
+                  typename boost::enable_if< xyz_array_get<Source> >::type* dummy1 = 0,
+                  typename boost::enable_if< xyz_ctor_set<Target> >::type* dummy2 = 0
+                 ) {
+  return Target(src[0], src[1], src[2]);
+}
+
 #if 0
 pcl::PointXYZ vector3f_to_point_xyz(Eigen::Vector3f ep) {
   pcl::PointXYZ p;
@@ -176,4 +237,4 @@ pcl::PointXYZ vector3f_to_point_xyz(Eigen::Vector3f ep) {
 
 } /* ca */
 
-#endif /* end of include guard: POINT_CONVERSION_HPP_LZDTPRFM */
+#endif /* end of include guard: POINT_CAST_HPP_EGWZH108 */
--- src/examples.cpp
+++ src/examples.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <iostream>
 #include "geom_cast/point_cast.hpp"
 
@@ -26,6 +27,20 @@ int main(int argc, char *argv[]) {
 
   Eigen::Vector3d v7 = ca::point_cast<Eigen::Vector3d>(v1);
   std::cerr << v7.x() << ", " << v7.y() << ", " << v7.z() << std::endl;
+
+  float * v8 = static_cast<float*>(malloc(sizeof(float)*3));
+  v8[0] = 2; v8[1] = 8; v8[2] = 0;
+  pcl::PointXYZ v9 = ca::point_cast<pcl::PointXYZ>(v8);
+  std::cerr << v9.x << ", " << v9.y << ", " << v9.z << std::endl;
+
+  float v10[3] = { 2., 8., 0.};
+  pcl::PointXYZ v11 = ca::point_cast<pcl::PointXYZ>(v10);
+  std::cerr << v11.x << ", " << v11.y << ", " << v11.z << std::endl;
+
+  cv::Point3_<float> v12 = ca::point_cast<cv::Point3_<float> >(v6);
+  std::cerr << v12.x << ", " << v12.y << ", " << v12.z << std::endl;
+
+
 
   return 0;
 }
